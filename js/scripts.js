dc206f02177dcf1295f9a9ec9be05a5e2411175b
lkneeshaw <lkneeshaw@vcd-ceb131-20.eastern.ewu.edu>
2017-02-22 09:32:22 -0800

    Randomized Image
    Added html and css from starter
    Added randomized image

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-// YOUR SCRIPTS GO HERE
+// Randomize Image
+
+// Store Image URLS
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get Random Number
+var limit = 3;
+var numRand = Math.floor( Math.random() * limit );
+console.log(numRand);
+
+// Show Images
+$('img').attr('src', eval('img' + numRand) );
